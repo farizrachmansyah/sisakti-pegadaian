4e7ca5046e3819221e170bf682fa31f017111557
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2021-05-11 06:29:02 +0700

    selesai membuat modal pengembalian untuk admin, membuat logic untuk menentukan button register atau kembali, membuat logic untuk mengambil data nomor soa dari table dan dipasang di form register ataupun pengembalian

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -1,3 +1,34 @@
+class SetButton {
+  constructor() {
+    this.tableRow = document.querySelectorAll('tbody tr');
+  }
+
+  setDataset() {
+    this.tableRow.forEach(row => {
+      // get every column values from each row
+      const rowCol = Array.from(row.children);
+
+      // edit button from last column values in row and get the first element child 
+      const editBtn = rowCol[8].firstElementChild;
+
+      // get column value form soa column and status column, then put them in variables
+      let noSOA = null;
+      let status = null;
+      rowCol.forEach((col, index) => {
+        if (index == 1) {
+          noSOA = col.innerHTML;
+        } else if (index == 6) {
+          status = col.innerHTML;
+        }
+      })
+
+      // create dataset for soa and status
+      editBtn.dataset.soa = noSOA;
+      editBtn.dataset.status = status;
+    })
+  }
+}
+
 class EventListener {
   constructor() {
     this.editBtn = document.querySelectorAll('.btn-edit');
@@ -6,12 +37,13 @@ class EventListener {
   showModal() {
     this.editBtn.forEach(button => {
       button.addEventListener('click', () => {
-        button.value === 'register' ? this.showRegisterModal(button.dataset.soa) : this.showKembaliModal();
+        button.dataset.status === 'register' ? this.showRegisterModal(button.dataset.soa) : this.showKembaliModal(button.dataset.soa);
       })
     });
   }
 
   showRegisterModal(noSOA) {
+    // ini masih belom diurus tombol submitnya
     const { value: formValues } = Swal.mixin({
       customClass: {
         confirmButton: 'sweetalert-btn-primary'
@@ -19,8 +51,8 @@ class EventListener {
     }).fire({
       title: 'Register',
       html: `
-        <div class="register-form">
-          <input class="register-form__soa" type="text" placeholder="SOA : ${noSOA}" disabled/>
+        <div class="admin-action-form">
+          <input class="admin-action-form__soa" type="text" placeholder="SOA : ${noSOA}" disabled/>
           <input type="date" />
           <input type="time" />
           <input type="text" placeholder="Nomor Register" />
@@ -37,10 +69,40 @@ class EventListener {
       }
     })
   }
+
+  showKembaliModal(noSOA) {
+    // ini masih belom diurus tombol submitnya
+    const { value: formValues } = Swal.mixin({
+      customClass: {
+        confirmButton: 'sweetalert-btn-primary'
+      }
+    }).fire({
+      title: 'Pengembalian',
+      html: `
+        <div class="admin-action-form">
+          <input class="admin-action-form__soa" type="text" placeholder="SOA : ${noSOA}" disabled/>
+          <input type="date" />
+          <input type="time" />
+          <input type="text" placeholder="Departemen" />
+          <input type="text" placeholder="Penerima" />
+        </div>
+      `,
+      confirmButtonText: 'Submit',
+      focusConfirm: false,
+      preConfirm: () => {
+        return [
+          document.getElementById('swal-input1').value,
+          document.getElementById('swal-input2').value
+        ]
+      }
+    })
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  const setData = new SetButton();
   const events = new EventListener();
 
+  setData.setDataset();
   events.showModal();
 });
