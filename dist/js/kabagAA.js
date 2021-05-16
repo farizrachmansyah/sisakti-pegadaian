72b3a81f5f71516bd8b6ace5ae5e176a49bfdec9
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2021-05-17 02:01:09 +0700

    berhasil ngebawa nilai soa per table row dan diambil lagi di halaman selanjutnya, dibawa lewat parameter url

--- dist/js/kabagAA.js
+++ dist/js/kabagAA.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
   tableRows.forEach(row => {
     row.addEventListener('click', el => {
       const thisRowData = el.target.parentElement;
-      console.log(thisRowData);
+      const theSOA = thisRowData.firstElementChild.innerText;
 
       // peernya adalah cari cara gimana caranya ngirim data dari element yang dibutuhkan ke halaman html lain untuk digunain data2nya
-      window.location.href = row.dataset.linked;
+      window.location.href = `${row.dataset.linked}?soa=${encodeURIComponent(theSOA)}`;
     })
   })
 });
