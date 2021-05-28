250995db2286c81f6a1fa9076d39ff517dbacc33
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2021-05-28 07:02:11 +0700

    nambahin bulan dan tahun di halaman report untuk jumlah realisasi per departemen

--- dist/js/report.js
+++ dist/js/report.js
@@ -69,31 +69,62 @@ class UI {
     contentTable.classList.add('report__content-table');
 
     content.innerHTML = `
-      <div class="dropdown show">
-        <a
-          class="btn dropdown-toggle"
-          href="#"
-          role="button"
-          id="dropdownMenuLink"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          Bulan
-        </a>
+      <div class="main__content-dropdown report__content-dropdown flex">
+        <div class="dropdown show">
+          <a
+            class="btn dropdown-toggle"
+            href="#"
+            role="button"
+            id="dropdownMenuLink"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Bulan
+          </a>
 
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-          <a class="dropdown-item">2016</a>
-          <a class="dropdown-item">2017</a>
-          <a class="dropdown-item">2018</a>
-          <a class="dropdown-item">2019</a>
-          <a class="dropdown-item">2020</a>
-          <a class="dropdown-item">2021</a>
-          <a class="dropdown-item">2022</a>
-          <a class="dropdown-item">2023</a>
-          <a class="dropdown-item">2024</a>
-          <a class="dropdown-item">2025</a>
-          <a class="dropdown-item">2026</a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item">Januari</a>
+            <a class="dropdown-item">Februari</a>
+            <a class="dropdown-item">Maret</a>
+            <a class="dropdown-item">April</a>
+            <a class="dropdown-item">Mei</a>
+            <a class="dropdown-item">Juni</a>
+            <a class="dropdown-item">Juli</a>
+            <a class="dropdown-item">Agustus</a>
+            <a class="dropdown-item">September</a>
+            <a class="dropdown-item">Oktober</a>
+            <a class="dropdown-item">November</a>
+            <a class="dropdown-item">Desember</a>
+          </div>
+        </div>
+
+        <div class="dropdown show">
+          <a
+            class="btn dropdown-toggle"
+            href="#"
+            role="button"
+            id="dropdownMenuLink"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Tahun
+          </a>
+
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item">2016</a>
+            <a class="dropdown-item">2017</a>
+            <a class="dropdown-item">2018</a>
+            <a class="dropdown-item">2019</a>
+            <a class="dropdown-item">2020</a>
+            <a class="dropdown-item">2021</a>
+            <a class="dropdown-item">2022</a>
+            <a class="dropdown-item">2023</a>
+            <a class="dropdown-item">2024</a>
+            <a class="dropdown-item">2025</a>
+            <a class="dropdown-item">2026</a>
+          </div>
         </div>
       </div>
     `;
