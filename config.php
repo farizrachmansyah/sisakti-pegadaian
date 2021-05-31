7b20c31abe153d368659f9c3bba6a94480986662
Afdrian Juarlin <afdrian23@gmail.com>
2021-05-31 12:39:17 +0700

    confignya lagi

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
-
 $db_host = "us-cdbr-east-04.cleardb.com:3306";
 $db_user = "bc824471867531";
 $db_pass = "ce77d49b";
 $db_name = "heroku_7198d5e8fe4265e";
+// $db_host = "localhost:3306";
+// $db_user = "root";
+// $db_pass = "";
+// $db_name = "sisakti";
 
 try {    
     //create PDO connection 
