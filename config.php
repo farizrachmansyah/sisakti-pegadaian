472bc47447359601a90e7539daf896a3582ca814
Afdrian Juarlin <afdrian23@gmail.com>
2021-05-31 12:36:35 +0700

    dbnya

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = "localhost:3306";
-$db_user = "root";
-$db_pass = "";
-$db_name = "sisakti";
+$db_host = "us-cdbr-east-04.cleardb.com:3306";
+$db_user = "bc824471867531";
+$db_pass = "ce77d49b";
+$db_name = "heroku_7198d5e8fe4265e";
 
 try {    
     //create PDO connection 
