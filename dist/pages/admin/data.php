7a53e9bd8edd7be6a3a275bfa726c0502c9bfa05
Afdrian Juarlin <afdrian23@gmail.com>
2021-06-02 02:06:41 +0700

    syalalla

--- dist/pages/admin/data.php
+++ dist/pages/admin/data.php
@@ -4,42 +4,42 @@ define('ROOTPATH', $_SERVER['DOCUMENT_ROOT']);
 require_once(ROOTPATH."/config.php");
 
 
-if(isset($_POST['register'])){
-    $user = $_SESSION["user"];
-    $userId = $user["usr_id"];
+// if(isset($_POST['register'])){
+//     $user = $_SESSION["user"];
+//     $userId = $user["usr_id"];
 
-    echo "alin";
-    print_r($_POST);
-    die();
+//     echo "alin";
+//     print_r($_POST);
+//     die();
 
-    if(!isset($_SESSION["user"])||$userId=''){
-      header("Location: ../../../login.php");
-    }
+//     if(!isset($_SESSION["user"])||$userId=''){
+//       header("Location: ../../../login.php");
+//     }
 
-    // filter data yang diinputkan
-    $soa_no = filter_input(INPUT_POST, 'soa-regis', FILTER_SANITIZE_STRING);
-    $ma = filter_input(INPUT_POST, 'ma', FILTER_SANITIZE_STRING);
-    // $soa_no = filter_input(INPUT_GET, 'soa_no', FILTER_SANITIZE_STRING);
-    $lastStatus = "Registered";
+//     // filter data yang diinputkan
+//     $soa_no = filter_input(INPUT_POST, 'soa-regis', FILTER_SANITIZE_STRING);
+//     $ma = filter_input(INPUT_POST, 'ma', FILTER_SANITIZE_STRING);
+//     // $soa_no = filter_input(INPUT_GET, 'soa_no', FILTER_SANITIZE_STRING);
+//     $lastStatus = "Registered";
     
-    // menyiapkan query
-    $sql = "UPDATE tbl_soa SET soa_lastupdate_by = :usr_id, soa_lastupdate_status = :last_status where soa_no = :soa_no";
-    $stmt = $db->prepare($sql);
+//     // menyiapkan query
+//     $sql = "UPDATE tbl_soa SET soa_lastupdate_by = :usr_id, soa_lastupdate_status = :last_status where soa_no = :soa_no";
+//     $stmt = $db->prepare($sql);
 
-    // bind parameter ke query
-    $params = array(
-        ":soa_no" => $soa_no,
-        ":usr_id" => $userId,
-        ":last_status" => $lastStatus
-    );    
+//     // bind parameter ke query
+//     $params = array(
+//         ":soa_no" => $soa_no,
+//         ":usr_id" => $userId,
+//         ":last_status" => $lastStatus
+//     );    
 
-    // eksekusi query untuk menyimpan ke database
-    $saved = $stmt->execute($params);
+//     // eksekusi query untuk menyimpan ke database
+//     $saved = $stmt->execute($params);
 
-    // jika query simpan berhasil, maka user sudah terdaftar
-    // maka alihkan ke halaman login
-    if($saved) header("Location: dashboard.php");
-}
+//     // jika query simpan berhasil, maka user sudah terdaftar
+//     // maka alihkan ke halaman login
+//     if($saved) header("Location: dashboard.php");
+// }
 
 
 if(isset($_POST['deptId'])){
